c7e4d123f3174bbe8112169261805cd7ef8be760
vagrant <vagrant@vagrant.vm>
2020-03-16 04:05:22 +0000

    trying different queries in def show_madlib_form

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-"""A madlib game that compliments its users."""
+"""Functions for Potluck Planner."""
 from model import User, Potluck, Dish, PotluckDish, connect_to_db, db
 from random import choice
 from flask import Flask, render_template, request, session, redirect
@@ -64,7 +64,9 @@ def show_madlib_form():
     
 
     if play_game == "yes":
+        #??select * from dishes where (potlucks.potluck_id = 1)
         potluck1Dishes = Dish.query.filter(Dish.potlucks.any(potluck_id=1)).all()
+
         # Same as below, but literal: dish_names = [item.dish_name for item in potluck1Dishes]
         dish_names = []
         for item in potluck1Dishes:
@@ -72,6 +74,12 @@ def show_madlib_form():
 
         return render_template("game.html", potluck1Dishes=potluck1Dishes, dish_names=dish_names)
     else:
+
+        # potlucks table query get one instance with id #2. 
+        # That instance has an attribute named dishes(defined in your model). 
+        # returns every dish associated with potluck #2 in the potlucks_dishes association table.
+        # potluck2Dishes2 = Potluck.query.get(2).dishes
+        
         potluck2Dishes = Dish.query.filter(Dish.potlucks.any(potluck_id=2)).all()
         # Same as below, but literal: dish_names = [item.dish_name for item in potluck1Dishes]
         dish_names = []
